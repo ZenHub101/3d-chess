8d78ea54b35d3b4c29a0948e5e47ca680087b56f
ABW Wijaya <abw.wijaya512@gmail.com>
2026-05-31 09:34:36 +0700

    add new piece + attribution

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.165.0/examples/jsm/loaders/GLTFLoader.js';
+
+// dom elements
+const coordsInfo = document.getElementById("coords");
+const turnInfo = document.getElementById("turn-text");
+const toast = document.getElementById("toast");
+
+// game variables
+let board = [];
