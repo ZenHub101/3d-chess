f5b5ab24accdbf3e6a60d90e61dcd7e3e4bf1825
ABW Wijaya <abw.wijaya512@gmail.com>
2026-05-31 10:10:31 +0700

    add basic things

--- main.js
+++ main.js
@@ -7,5 +7,73 @@ const coordsInfo = document.getElementById("coords");
 const turnInfo = document.getElementById("turn-text");
 const toast = document.getElementById("toast");
 
+// self-explanatory
+class Piece {
+  constructor(type, color) {
+    this.type = type;
+    this.color = color;
+  }
+}
+
 // game variables
-let board = [];
+let board = Array(8).fill().map(() =>
+  Array(8).fill().map(() =>
+    Array(8).fill(null)
+  )
+);
+let currentPlayer = 1; // 1 = white; 2 = black
+const size = 8;
+const selected = {
+    x: 0,
+    y: 0,
+    z: 0
+};
+
+// scene
+const scene = new THREE.Scene();
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+    60, // FOV
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    anitalias: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(0, 0, 0);
+controls.minDistance = 5;
+controls.maxDistance = 12;
+controls.update();
+
+// ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(12, 12, 12);
+scene.add(directionalLight);
+
+
+
+// --- function definitions ---
+
+// get/place pieces
+function getPiece(x, y, z) {
+  if (x < 0 || x > 7 || y < 0 || y > 7 || z < 0 || z > 7) return null;
+  return board[x][y][z];
+}
+function setPiece(x, y, z, piece) {
+  board[x][y][z] = piece;
+}
